15111953e0de256f976c9e0ac2605c5c2a5c6371
Felipe Correia <felipedreamcorreia@hotmail.com>
2021-12-23 00:00:44 -0300

    feature: sistema de aluguel de carros finalizado

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -1,6 +1,7 @@
 package model.services;
 
 import model.entities.CarRental;
+import model.entities.Invoice;
 
 public class RentalService {
 	
@@ -28,6 +29,9 @@ public class RentalService {
 		else {
 			basicPayment = Math.ceil(hours / 24) * pricePerDay;
 		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
 	}
-
 }
